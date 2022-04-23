768c3fc40aa1d86ce90dc95570d3d567bd3d6712
Julian110101 <57191513+IngenieroJulian@users.noreply.github.com>
2022-04-23 14:47:21 -0500

    Add files via upload

---
+++ springboot-api-rest/src/test/java/com/julianbackend/app/SpringbootApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianbackend.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
